ee4054c769861fca032415edb43d177f8aed605a
victor <victor.outtes@gmail.com>
2021-06-30 16:55:43 -0300

    get_dataframe now accepts download just one page

--- sap_rfc_data_collector/sap.py
+++ sap_rfc_data_collector/sap.py
@@ -2,6 +2,7 @@ import pandas as pd
 from pyrfc import ABAPApplicationError, ABAPRuntimeError, LogonError, CommunicationError
 
 from .connection import SAPConnection
+from typing import List
 
 
 class SAP:
@@ -32,18 +33,24 @@ class SAP:
             df.loc[j] = resultado.split('¬')
         return df
 
-    def get_dataframe(self, table: str, columns: list, where: str = None, page_size: int = 1000) -> pd.DataFrame:
+    def get_dataframe(self,
+                      table: str,
+                      columns: List[str],
+                      where: str = None,
+                      page_size: int = 1000,
+                      page: int = None) -> pd.DataFrame:
         """
         Get data from SAP table in pandas dataframe format.
         :param table: SAP table name.
         :param columns: list of columns desired.
         :param where: where clause (optional)
         :param page_size: number of rows per query. Optional. Default: 1000.
+        :param page: page. Optional. Put None for all data. Put some integer to get that pagination result. Default: None.
         :return: pandas dataframe.
         """
         fields = []
         where_clause = []
-        page = 1
+        rows_collected = 0
         df = pd.DataFrame(columns=columns)
 
         if where:
@@ -52,6 +59,41 @@ class SAP:
         if columns:
             fields = [{"FIELDNAME": f} for f in columns]
 
+        if page:
+            try:
+                connection = self.connection.get_connection()
+
+                start = (page - 1) * page_size
+                limit = page_size
+                result = connection.call('RFC_READ_TABLE',
+                                         QUERY_TABLE=table,
+                                         DELIMITER='¬',
+                                         FIELDS=fields,
+                                         OPTIONS=where_clause,
+                                         ROWSKIPS=start,
+                                         ROWCOUNT=limit)
+                df = df.append(self._to_dataframe(result['DATA'], columns))
+                connection.close()
+                rows_collected += df.shape[0]
+                print(f'Approximated rows collected...{rows_collected}')
+            except CommunicationError:
+                if df.empty:
+                    df['error'] = ['Could not connect to server.']
+                else:
+                    df['error'] = 'Could not connect to server.'
+            except LogonError:
+                if df.empty:
+                    df['error'] = ['Could not log in. Wrong credentials?']
+                else:
+                    df['error'] = 'Could not log in. Wrong credentials?'
+            except (ABAPApplicationError, ABAPRuntimeError):
+                if df.empty:
+                    df['error'] = ['An error occurred.']
+                else:
+                    df['error'] = 'An error occurred.'
+            return df
+
+        page = 1
         while True:
             try:
                 connection = self.connection.get_connection()
@@ -67,7 +109,8 @@ class SAP:
                                          ROWCOUNT=limit)
                 df = df.append(self._to_dataframe(result['DATA'], columns))
                 connection.close()
-                print(f'Approximated rows collected...{page * page_size}')
+                rows_collected += df.shape[0]
+                print(f'Approximated rows collected...{rows_collected}')
                 if len(result['DATA']) < page_size:
                     break
                 page += 1
