f1619013f2116e2d040f0bcfb45d5786459bee3b
victor <victor.outtes@gmail.com>
2021-07-01 14:35:05 -0300

    convert get_data_df with generator

--- sap_rfc_data_collector/sap.py
+++ sap_rfc_data_collector/sap.py
@@ -2,7 +2,7 @@ import pandas as pd
 from pyrfc import ABAPApplicationError, ABAPRuntimeError, LogonError, CommunicationError
 
 from .connection import SAPConnection
-from typing import List
+from typing import List, Generator
 
 
 class SAP:
@@ -33,71 +33,23 @@ class SAP:
             df.loc[j] = resultado.split('¬')
         return df
 
-    def get_dataframe(self,
-                      table: str,
-                      columns: List[str],
-                      where: str = None,
-                      page_size: int = 1000,
-                      page: int = None) -> pd.DataFrame:
-        """
-        Get data from SAP table in pandas dataframe format.
-        :param table: SAP table name.
-        :param columns: list of columns desired.
-        :param where: where clause (optional)
-        :param page_size: number of rows per query. Optional. Default: 1000.
-        :param page: page. Optional. Put None for all data. Put some integer to get that pagination result. Default: None.
-        :return: pandas dataframe.
-        """
+    def get_data_df(self,
+                    table: str,
+                    columns: List[str],
+                    where: str = None,
+                    page_size: int = 1000) -> Generator[pd.DataFrame, None, None]:
         fields = []
         where_clause = []
-        rows_collected = 0
         df = pd.DataFrame(columns=columns)
-
         if where:
             where_clause = [{"TEXT": where}]
-
         if columns:
             fields = [{"FIELDNAME": f} for f in columns]
-
-        if page:
-            try:
-                connection = self.connection.get_connection()
-
-                start = (page - 1) * page_size
-                limit = page_size
-                result = connection.call('RFC_READ_TABLE',
-                                         QUERY_TABLE=table,
-                                         DELIMITER='¬',
-                                         FIELDS=fields,
-                                         OPTIONS=where_clause,
-                                         ROWSKIPS=start,
-                                         ROWCOUNT=limit)
-                df = df.append(self._to_dataframe(result['DATA'], columns))
-                connection.close()
-                rows_collected += df.shape[0]
-                print(f'Approximated rows collected...{rows_collected}')
-            except CommunicationError:
-                if df.empty:
-                    df['error'] = ['Could not connect to server.']
-                else:
-                    df['error'] = 'Could not connect to server.'
-            except LogonError:
-                if df.empty:
-                    df['error'] = ['Could not log in. Wrong credentials?']
-                else:
-                    df['error'] = 'Could not log in. Wrong credentials?'
-            except (ABAPApplicationError, ABAPRuntimeError):
-                if df.empty:
-                    df['error'] = ['An error occurred.']
-                else:
-                    df['error'] = 'An error occurred.'
-            return df
 
         page = 1
         while True:
             try:
                 connection = self.connection.get_connection()
-
                 start = (page - 1) * page_size
                 limit = page_size
                 result = connection.call('RFC_READ_TABLE',
@@ -107,10 +59,8 @@ class SAP:
                                          OPTIONS=where_clause,
                                          ROWSKIPS=start,
                                          ROWCOUNT=limit)
-                df = df.append(self._to_dataframe(result['DATA'], columns))
                 connection.close()
-                rows_collected += df.shape[0]
-                print(f'Approximated rows collected...{rows_collected}')
+                yield self._to_dataframe(result['DATA'], columns)
                 if len(result['DATA']) < page_size:
                     break
                 page += 1
@@ -132,5 +82,3 @@ class SAP:
                 else:
                     df['error'] = 'An error occurred.'
                 break
-
-        return df
