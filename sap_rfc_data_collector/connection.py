d37cca4253228227fafef0d09f7844cdea6bbfc8
victor <victor.outtes@gmail.com>
2021-09-23 16:41:19 -0300

    optional connection params

--- sap_rfc_data_collector/connection.py
+++ sap_rfc_data_collector/connection.py
@@ -3,13 +3,13 @@ from pyrfc import Connection
 
 class SAPConnection:
     def __init__(self,
-                 host: str,
-                 service: str,
-                 sysname: str,
                  client: str,
                  lang: str,
                  user: str,
                  password: str,
+                 host: str = None,
+                 service: str = None,
+                 sysname: str = None,
                  group: str = None):
         self.host = host
         self.service = service
@@ -21,24 +21,13 @@ class SAPConnection:
         self.password = password
 
     def get_connection(self) -> Connection:
-        if self.group:
-            return Connection(
-                user=self.user,
-                passwd=self.password,
-                mshost=self.host,
-                msserv=self.service,
-                group=self.group,
-                sysid=self.sysname,
-                client=self.client,
-                lang=self.lang
-            )
-        else:
-            return Connection(
-                user=self.user,
-                passwd=self.password,
-                mshost=self.host,
-                msserv=self.service,
-                sysid=self.sysname,
-                client=self.client,
-                lang=self.lang
-            )
+        return Connection(
+            user=self.user,
+            passwd=self.password,
+            mshost=self.host,
+            msserv=self.service,
+            group=self.group,
+            sysid=self.sysname,
+            client=self.client,
+            lang=self.lang
+        )
